017eac3e98a9de5ee59df6e2721b0fb97a1a6f95
StuFF mc <mc@stuffmc.com>
2011-09-18 23:19:24 +0200

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,27 @@
+I'm always using the "Hold for Developer Release" method for my apps. Sadly, this is the one case where Apple won't send me an email to let me know :( So I had to be creative.
+
+How to use this?
+===============
+First of all, you'll need "mechanize". Install with "gem install mechanize".
+
+At the same level as itc.rb, you should have a file called "itc-pwd.rb" (or whatever, wherever, but then change the first "require") containing those lines: 
+
+USERNAME = "foo"        ==> Your iTunes Connect Username
+PASSWORD = "bar"        ==> Your iTunes Connect Password
+APP      = "Disk Alarm" ==> This is the App we should check
+
+You shouldn't -- in most case -- have to modify itc.rb
+
+Now go in the Terminal, where itc.rb is and type
+
+chmod u+x itc.rb
+
+Now type "./itc.rb" which will launch the script in an endless loop. To quit the app, use "Ctrl C" twice.
+
+The first time, the app will create a "YOUR APP.txt" file with the status ("Waiting for Review" for ex.)
+
+As soon as this status changes, the app will display a message, make beeps, and speak the message.
+
+It will *repeat* this in a loop ;-) until you break and restart the script. This is to ensure the "It's okay, I'll do something factor! You basically start the script once per Status change.
+
+PS: Obviously, this is a *very* basic solution, but for now, it's enough for me ;) Feel free to fork me!
---
+++ itc.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+DIR      = File.dirname(__FILE__) # change this if the other files than itc.rb aren't in the same directory. Not in most cases
+require File.join(DIR, 'itc-pwd.rb') 
+require 'mechanize'
+
+# You shouldn't change these but if Apple changes, we'll need to change it ;-)
+ITC = "https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/wo/"
+LOGIN = "6.0.9.3.5.2.1.1.3.1.1?theAccountName=#{USERNAME}&theAccountPW=#{PASSWORD}"
+APPS = "Manage Your Applications"
+ALERT = "Your Status for #{APP} changed on iTunes Connect"
+changed = false
+
+def alarm
+  puts ALERT
+  puts "\a\a\a\a\a"
+  system("say '#{ALERT}' -v alex")
+  true
+end
+
+loop do
+  if changed
+    # this is to ensure the "It's okay, I'll do something factor!"
+    changed = alarm
+    # You'll have to break and restart the app for it to stop! ;-)
+    sleep 2
+  else
+    puts "Login to iTunes Connect..."
+    agent = Mechanize.new
+    page = agent.get(ITC+LOGIN) # Loging
+    puts "Clicking '#{APPS}'..."
+    page = agent.page.links.find { |l| l.text == APPS }.click # clicking "Manage Your Applications"
+    puts "Clicking '#{APP}'..."
+    page = agent.page.links.find { |l| l.text =~ /#{APP}/ }.click # clicking the App you want to track
+
+    puts "Checking Status..."
+    # Warning VERY fuzzy way of looking for the "status" <span>. There's always mostly version of your app: the current one and the next one. If it's the first one you might want to change the 5 to a 1. Then we check the content of this span, which should be something else than what we have in a file located in the same directory as the script.
+    status = page.search(".//div[@class='version-container']/div/p/span")[5].content().strip()
+    puts "Status: " + status
+
+    # We'll now check the "YOUR APP.txt" file to see if we have a new status
+    file = File.join(DIR, APP+'.txt')
+    if File.exists?(file)
+      if File.open(file, 'rb') { |f| f.read } != status
+        changed = alarm
+      end
+    else
+      File.open(file, 'w') {|f| f.write(status) }
+    end
+  end
+end
+
